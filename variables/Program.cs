2be4010d0251b384ab10b604104a053991f22586
mohammedaladhary <caladhary@gmail.com>
2023-05-31 14:21:59 +0400

    input and call variables

---
+++ variables/Program.cs
@@ -0,0 +1,48 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //double i = 6.5;
+        //char a = 'b';
+        //Console.WriteLine(i + "" +a);
+
+
+        //Console.WriteLine("Enter the first number:");
+        //int a = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine("Enter the second number:");
+        //int b = Convert.ToInt32(Console.ReadLine());
+
+
+        //Console.WriteLine("a+b=" + (a + b));
+        //Console.WriteLine("a-b=" + (a - b));
+        //Console.WriteLine("b-a=" + (b - a));
+        //Console.WriteLine("a/b=" + (a / b));
+        //Console.WriteLine("b/a=" + (b / a));
+        //Console.WriteLine("a*b=" + (a * b));
+        //Console.WriteLine("a%b=" + (a % b));
+        //Console.WriteLine("b%a=" + (b % a));
+
+        Console.WriteLine("Enter the first string:");
+        string a = Console.ReadLine();
+
+        Console.WriteLine("Enter the second string:");
+        string b = Console.ReadLine();
+
+        Console.WriteLine("<==Swap==>");
+        (a, b) = (b, a);
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+
+
+
+
+
+
+
+
+    }
+}
