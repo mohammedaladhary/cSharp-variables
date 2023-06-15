a2bfc7cacf6e908f22e4e528715f7d4d9b7d8cc3
mohammedaladhary <caladhary@gmail.com>
2023-06-15 14:46:07 +0400

    Encapsulation

---
+++ Access_modifiers/ClassInMain.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_modifiers
+{
+    internal class Person
+    {
+        // Declare private fields
+        public string name { get; set; }
+        public int age { get; set; }
+        public string address { get; set; }
+        private bool isHeHasFamily;
+
+        // Define public methods to set and get the values of the fields
+        public Person(string name, string address, int age)
+        {
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
+
+        public void family()
+        {
+            isHeHasFamily = true;
+            Console.WriteLine("he has a family");
+        }
+
+      
+        string personName()
+            {
+                this.name = "Ahmed";
+                return this.name;
+            }
+      
+        int personAge()
+        {
+            this.age = 24;
+            return this.age;
+        }
+
+        string setAddress()
+        {
+            this.address = "Muscat";
+            return this.address;
+        }
+    }
+}
---
+++ Access_modifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Access_modifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ahmed", "Muscat", 24);
+            Console.WriteLine(person.name);
+            Console.WriteLine(person.address);
+            Console.WriteLine(person.age);
+        }
+    }
+}
