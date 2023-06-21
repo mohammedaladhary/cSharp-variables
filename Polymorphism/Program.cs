d36ab46b0bec6ba6e8c23cfb98765598a79a0bea
mohammedaladhary <caladhary@gmail.com>
2023-06-21 10:48:31 +0400

    shape exercise

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Shape[] design = new Shape[4];
+                design[0] = new Circle(2);
+                design[1] = new Rectangle(3, 5);
+                design[2] = new Triangle(4, 7);
+                design[3] = new Circle(5);
+
+
+            for (int i = 0; i < design.Length; i++)
+                {
+                Shape shape = design[i];
+                Console.WriteLine("Area of " + shape.GetType().Name + " is " + shape.CalculateArea());
+                }
+            Console.WriteLine("-------------------------------------------");
+
+        }
+    }
+}
---
+++ Polymorphism/Shape.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+
+    internal class Circle : Shape
+    {
+        public double Radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+            }
+    }
+
+    internal class Rectangle : Shape
+    {
+        public double Width;
+        public double Height;
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    internal class Triangle : Shape
+    {
+        public double Width;
+        public double Height;
+
+        public Triangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * Width * Height;
+        }
+    }
+}
