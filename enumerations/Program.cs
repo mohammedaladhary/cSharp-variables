3fed282a47493084f85d0eef6928caef4dd35209
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-05 12:35:20 +0400

    enumeration gender and carType

---
+++ enumerations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace enumerations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            User user1 = new User("Aladhary", 1, Gender.MALE, CarType.PREMIUM);
+            User user2 = new User("Osama", 1, Gender.MALE, CarType.PREMIUM);
+            User user3 = new User("Salim", 1, Gender.MALE, CarType.PREMIUM);
+
+            foreach (Gender gender in Enum.GetValues(typeof(Gender)))
+            {
+                Console.WriteLine(gender.ToString());
+            }
+
+            Console.WriteLine((int)Gender.FEMALE);
+            Console.WriteLine((int)CarType.VAN);
+            string description = getEnumDescription(CarType.BASIC);
+            Console.WriteLine(description);
+
+
+        }
+
+        private static string getEnumDescription(Enum enums)
+        {
+            var field = enums.GetType().GetField(enums.ToString());
+            var attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+            return attribute != null ? attribute.Description : enums.ToString();
+        }
+    }
+}
---
+++ enumerations/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumerations
+{
+    internal class User
+    {
+        public string name { get; set; }
+        Gender gender { get; set; }
+        CarType carType { get; set; }
+
+        public User(string name, int id, Gender gender, CarType carType)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.carType = carType;
+        }
+    }
+
+        enum Gender
+        {
+            MALE,
+            FEMALE,
+            OTHER
+        }
+        enum CarType
+        {
+            BASIC,
+            PREMIUM,
+            VAN
+        }
+    }
